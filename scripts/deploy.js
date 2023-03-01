c3e17ad5238344932539e81d24fb5a62f0240919
Ivan <retouch226@gmail.com>
2023-03-01 19:15:16 +0200

    feat: chainLinkContract deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,15 +11,24 @@ async function main() {
   await stakingContract.deployed();
   /////
   const Cardano = await hre.ethers.getContractFactory("Cardano", tokensOwner);
-  const cardanoContract = await Cardano.deploy(
-    2000,
-    hre.ethers.utils.parseUnits("1000", "wei"),
-    { value: hre.ethers.utils.parseEther("10") }
-  );
+  const cardanoContract = await Cardano.deploy();
   await cardanoContract.deployed();
+  /////
+  const Zil = await hre.ethers.getContractFactory("Zil", tokensOwner);
+  const zilContract = await Zil.deploy();
+  await zilContract.deployed();
+  /////
+  const ChainLink = await hre.ethers.getContractFactory(
+    "ChainLink",
+    tokensOwner
+  );
+  const chainLinkContract = await ChainLink.deploy();
+  await chainLinkContract.deployed();
 
   console.log("stakingContract address", stakingContract.address);
   console.log("cardanoContract address", cardanoContract.address);
+  console.log("zilContract address", zilContract.address);
+  console.log("chainLinkContract address", chainLinkContract.address);
 }
 
 main().catch((error) => {
