89ed8ba662f21a5e70d94f76188ef6ca4cb3b01d
Ivan <retouch226@gmail.com>
2023-03-02 22:21:08 +0200

    feat: Market component and Market.module.scss, write styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import "./App.css";
+import "./App.scss";
 import Main from "./pages/Main";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Main>
-        <Header />
-      </Main>
+      <Header />
+      <Main></Main>
     </div>
   );
 }
