b4525587fa7275572f1df0b94f5eb68e61ebb023
Ivan <retouch226@gmail.com>
2023-03-02 22:34:16 +0200

    feat: add title and subtitles  to Market component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.scss";
 import Main from "./pages/Main";
 import Header from "./components/Header/Header";
+import Market from "./components/Market/Market";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main></Main>
+      <Main>
+        <Market />
+      </Main>
     </div>
   );
 }
--- src/components/Market/Market.jsx
+++ src/components/Market/Market.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import cl from "./Market.module.scss";
 
 const Market = () => {
-  return <div></div>;
+  return (
+    <div className={cl.market}>
+      <div className={cl.market__container}>
+        <div className={cl.market__title}>
+          <p>Ethereum market</p>
+        </div>
+        <div className={cl.market__content}>
+          <div className={cl.market__item}>
+            <p>Asset</p>
+            <p>Symbol</p>
+            <p>Price</p>
+            <p>Supply</p>
+            <p>Apy</p>
+            <button>Stake</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Market;
