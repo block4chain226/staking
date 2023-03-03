ed339616c8e14ef567cd5d5f7ed2bb180aa6a6ff
Ivan <retouch226@gmail.com>
2023-03-03 23:25:51 +0200

    test

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import cl from "./Modal.module.scss";
+const { ethers } = require("ethers");
 
 const Modal = ({ symbol, contract }) => {
+  const [stakeAmount, setStakeAmount] = useState("");
+
+  async function stakeTokens(e) {
+    e.preventDefault();
+    if (stakeAmount !== "" && stakeAmount.length > 0) {
+      await contract.stakeTokens(symbol, stakeAmount, {
+        value: ethers.utils.parseUnits("1000", "wei"),
+      });
+      console.log(await contract.getPositionById(0));
+    }
+  }
   return (
     <div className={cl.modal}>
       <div className={cl.modal__button}></div>
@@ -9,12 +21,16 @@ const Modal = ({ symbol, contract }) => {
         <form>
           <label style={{ display: "flex", justifyContent: "space-around" }}>
             <p>Tokens amount</p>
-            <input value={symbol} />
+            <input
+              value={stakeAmount}
+              onChange={(e) => setStakeAmount(e.target.value)}
+            />
             <button
               style={{
                 borderRadius: "10px",
                 padding: "5px 15px",
               }}
+              onClick={(e) => stakeTokens(e)}
             >
               Stake
             </button>
