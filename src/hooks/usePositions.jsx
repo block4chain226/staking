b6f1b5b4a54ebd8860bfdb0503be2b0091ddb06b
Ivan <retouch226@gmail.com>
2023-03-04 20:49:59 +0200

    feat: usePositions, get all user Positions

--- src/hooks/usePositions.jsx
+++ src/hooks/usePositions.jsx
@@ -2,47 +2,53 @@ import React, { useEffect, useState } from "react";
 
 const usePositions = (contract, account) => {
   const [allPositions, setAllPositions] = useState([]);
+  const [allIds, setAllIds] = useState("");
 
   const getAllUserPositionsId = async () => {
     let positionsIdsPromises = await contract.getPositionsIdsByAddress(account);
-    console.log(
-      "🚀 ~ file: usePositions.jsx:8 ~ getAllUserPositionsId ~ positionsIdsPromises:",
-      positionsIdsPromises
-    );
-
     const ids = positionsIdsPromises.map((item) => {
-      let obj = item._hex;
-      const vasa = Object.values(obj.split());
-      return vasa[0];
+      return Object.values(item._hex.split())[0];
     });
-    console.log("🚀 ~ file: usePositions.jsx:12 ~ ids ~ ids:", ids);
-
-    // positionsIdsPromises = positionsIdsPromises.map((item) => {
-    //   return item.toString();
-    // });
-    // return positionsIdsPromises;
+    setAllIds(ids);
+    // return ids;
   };
 
   const getAllUserPositions = async () => {
-    const allUserPositionsId = getAllUserPositionsId();
-    Promise.all(
-      allUserPositionsId.then((data) =>
-        data.map(async (item) => {
-          const position = await contract.getPositionById(item.toString());
-          setAllPositions((prev) => [...prev, position]);
-        })
-      )
-    );
+    console.log(allIds);
 
-    return [allPositions];
+    // Promise.all(
+    //   allIds.then((data) =>
+    allIds.map(async (item) => {
+      const position = await contract.getPositionById(item);
+      setAllPositions((prev) => [...prev, position]);
+    });
+    //   )
+    // );
+
+    // Promise.all(
+    //   allUserPositionsId.then((data) =>
+    //     data.map(async (item) => {
+    //       const position = await contract.getPositionById(item.toString());
+    //       setAllPositions((prev) => [...prev, position]);
+    //     })
+    //   )
+    // );
+
+    // return [allPositions];
   };
 
   useEffect(() => {
     if (contract && account) {
-      debugger;
       getAllUserPositionsId();
     }
   }, []);
+
+  useEffect(() => {
+    getAllUserPositions();
+  }, [allIds]);
+  useEffect(() => {
+    console.log(allPositions);
+  }, [allPositions]);
   return <div></div>;
 };
 
